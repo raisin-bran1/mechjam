3859a7a90cda2543a6549a795c961bba7b394e74
BuildTools <unconfigured@null.spigotmc.org>
2024-07-16 22:07:05 -0700

    enemy spawning (doesnt work)

--- Assets/World/Worldbuilder.cs
+++ Assets/World/Worldbuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,13 @@ using UnityEngine.Tilemaps;
 
 public class Worldbuilder : MonoBehaviour
 {
-    private float spawn_time = 0f, spawn_length = 100, spawn_interval;
+    private float spawnTime = 0f, spawnLength, spawnInterval;
     private Tile tile;
     private Tilemap terrain;
+    private float difficulty;
+    //rate in enemies per t, acceleration in enemies per t per t, chance to accelerate rate rather than increment difficulty; difficulty starts at .5 and each next stronger sheep starts appearing at each integer
+    public float startRate, rateAcc, rateChance, diffIncrement;
+    public float timeUnit;
     public GameObject player;
     public GameObject sheep;
     public GameObject bigSheep;
@@ -20,25 +25,38 @@ public class Worldbuilder : MonoBehaviour
         tile = Resources.Load<Tile>("Temp_tile");
         terrain = GameObject.Find("Terrain").GetComponent<Tilemap>();
         placeGround();
-        spawnSheep(20);
-        spawn_interval = spawn_length + Random.Range(-1f, 1f);
+        spawnSheep(40);
+        spawnLength = timeUnit / startRate;
+        difficulty = 0.5f;
+        spawnInterval = Math.Max(1.0f, spawnLength + UnityEngine.Random.Range(-6f, 6f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawn_time += Time.deltaTime;
-        if (spawn_time >= spawn_interval)
+        spawnTime += Time.deltaTime;
+        if (spawnTime >= spawnInterval)
         {
-            if (Random.Range(0f, 2f) >= 1)
+            if (UnityEngine.Random.Range(0f, 2f) >= 1)
             {
-                spawnSheep(20);
+                spawnSheep(40);
             } else
             {
-                spawnSheep(-20);
+                spawnSheep(-40);
             }
-            spawn_time -= spawn_interval;
-            spawn_interval = spawn_length + Random.Range(-1f, 1f);
+            spawnTime -= spawnInterval;
+            spawnInterval = Math.Max(1.0f, spawnLength + UnityEngine.Random.Range(-6f, 6f));
+        }
+        if (Time.fixedTime / timeUnit - (Time.fixedTime - Time.deltaTime) / timeUnit == 1)
+        {
+            if (UnityEngine.Random.Range(0.0f, 1.0f) < rateChance)
+            {
+                startRate += rateAcc;
+                spawnLength = timeUnit / startRate;
+            } else
+            {
+                difficulty += diffIncrement;
+            }
         }
     }
     public void placeGround()
@@ -52,11 +70,25 @@ public class Worldbuilder : MonoBehaviour
 
     public void spawnSheep(float x)
     {
-        //sheep.GetComponent<EnemyMove>().speed = Random.Range(2,4);
-        //Instantiate(sheep, new Vector3(x, -4.5f, 0), Quaternion.identity);
-        //Instantiate(bigSheep, new Vector3(x, -4.5f, 0), Quaternion.identity);
-        //flyingSheep.GetComponent<EnemyMove>().speed = Random.Range(3, 5);
-        //Instantiate(flyingSheep, new Vector3(x, 0, 0), Quaternion.identity);
-        Instantiate(astralSheep, new Vector3(x, 0, 0), Quaternion.identity);
+        float r = UnityEngine.Random.Range(0f, difficulty) % 5.0f;
+        if (r <= 1.0f)
+        {
+            sheep.GetComponent<EnemyMove>().speed = UnityEngine.Random.Range(2, 4);
+            Instantiate(sheep, new Vector3(x, -4.5f, 0), Quaternion.identity);
+        } else if (r <= 2.0f)
+        {
+            flyingSheep.GetComponent<EnemyMove>().speed = UnityEngine.Random.Range(3, 5);
+            Instantiate(flyingSheep, new Vector3(x, 0, 0), Quaternion.identity);
+        } else if (r <= 3.0f)
+        {
+            Instantiate(bigSheep, new Vector3(x, -4.0f, 0), Quaternion.identity);
+        } else if (r <= 4.0f) {
+            Instantiate(astralSheep, new Vector3(x, 0, 0), Quaternion.identity);
+        } else
+        {
+            sheep.GetComponent<EnemyMove>().speed = UnityEngine.Random.Range(2, 4);
+            Instantiate(sheep, new Vector3(x, -4.5f, 0), Quaternion.identity);
+            Instantiate(sheep, new Vector3(-x, -4.5f, 0), Quaternion.identity);
+        }
     }
 }
